3a1145e70417c0faa79d7f7df32eeaa2d2e87e3e
TerminalEagle <affan.shaikh2309@gmail.com>
2024-05-11 16:03:09 +0530

    Fixed 404 error on site reload

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const path = require("path");
 app.use(express.json());
 app.use(cors());
-
 require("./connect");
 
 const productsRoutes = require("./routes/products");
 const cartRoutes = require("./routes/cart");
 const authRoutes = require("./routes/auth");
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.use((req, res, next) => {
+  if (req.url.startsWith("/api")) {
+    next();
+  } else {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+  }
+});
 
 app.use("/api", productsRoutes);
 app.use("/api", cartRoutes);
